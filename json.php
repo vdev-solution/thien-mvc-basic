d09c09331de94ecf0d1df3e7c69d7b06d0d2e01a
thien tong <thientong73@gmail.com>
2018-03-12 17:38:05 +0700

    update

--- json.php
+++
@@ -1,9 +0,0 @@
-<?php  
-    $array = array(
-        "name" => "Nguyễn Văn Cường",
-        "email" => "TheHalfHeart@gmail.com",
-        "website" => "freetuts.net" 
-    );
-     
-    echo json_encode($array);
-?>
